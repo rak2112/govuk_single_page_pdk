8891b502ab2828b02286d9d79b1409a4f46c5bf5
Ben Ripley <ben.ripley@digi2al.co.uk>
2016-10-05 15:34:36 +0100

    feat(collapsible): Add collapsible component, styles, tests and docs. Deprecate show/hide pane.

--- src/modules/components/data-visualisation/data-visualisation.ts
+++ src/modules/components/data-visualisation/data-visualisation.ts
@@ -1,5 +1,12 @@
 import 'angular-slugify';
 
+const module = angular.module('govuk-single-page-pdk.components.data-visualisation', [
+  'ngAnimate',
+  'slugifier'
+]);
+
+// Legacy
+
 import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 import { ContactInfoComponent} from './contact-info/contact-info.component';
 import { SummaryItemComponent} from './summary-item/summary-item.component';
@@ -8,16 +15,11 @@ import { FiltersComponent } from './filters/filters.component';
 import { TabComponent } from './tab/tab.component';
 import { TabsPaneComponent} from './tabs-pane/tabs-pane.component';
 import { TabsetComponent} from './tab/tabset.component';
-import { ShowHidePaneComponent} from './show-hide-pane/show-hide-pane.component';
 import { ProgressListComponent } from './progress-list/progress-list.component';
 import { DatasetComponent } from './dataset/dataset.component';
 import { CaseMarkersComponent } from './case-markers/case-markers.component';
 
-const module = angular.module('govuk-single-page-pdk.components.data-visualisation', [
-    'ngAnimate',
-    'slugifier'
-])
-
+module
   .component('govPersonalDetails', PersonalDetailsComponent)
   .component('govContactInfo', ContactInfoComponent)
   .component('govSummaryItem', SummaryItemComponent)
@@ -26,9 +28,17 @@ const module = angular.module('govuk-single-page-pdk.components.data-visualisati
   .component('govTab', TabComponent)
   .component('govTabsPane', TabsPaneComponent)
   .component('govTabset', TabsetComponent)
-  .component('govShowHidePane', ShowHidePaneComponent)
   .component('govProgressList', ProgressListComponent)
   .component('govDataset', DatasetComponent)
   .component('govCaseMarkers', CaseMarkersComponent);
+
+// Latest
+
+import { CollapsibleComponent } from './collapsible/collapsible.component';
+import { CollapseDirective } from './collapsible/collapse.directive';
+
+module
+  .component('pdkCollapsible', CollapsibleComponent)
+  .directive('collapse', CollapseDirective);
 
 export default module.name;
